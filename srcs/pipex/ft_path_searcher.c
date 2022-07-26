b60a08250292f41b41ef9c3773d90768e88cbb89
FateEvent <fabio.venturini.1989@gmail.com>
2022-07-26 11:38:42 +0200

    correcting errors, pushing changes

--- srcs/pipex/ft_path_searcher.c
+++ srcs/pipex/ft_path_searcher.c
@@ -3,30 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_path_searcher.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: albaur <albaur@student.42.fr>              +#+  +:+       +#+        */
+/*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 16:24:11 by faventur          #+#    #+#             */
-/*   Updated: 2022/07/19 13:49:07 by albaur           ###   ########.fr       */
+/*   Updated: 2022/07/26 11:36:45 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static char	*ft_path_searcher_free(t_searcher *s)
-{
-	ft_arr_freer(s->paths);
-	ft_arr_freer(s->cmd_args);
-	return (NULL);
-}
 
 char	*ft_path_searcher(char *cmd)
 {
 	t_searcher	s;
 
 	s.i = -1;
-	s.cmd_args = ft_split(cmd, ' ');
-	if (!s.cmd_args)
-		return (NULL);
 	s.env_path = ft_getenv("PATH");
 	s.paths = ft_split(s.env_path, ':');
 	free(s.env_path);
@@ -34,11 +24,11 @@ char	*ft_path_searcher(char *cmd)
 		return (NULL);
 	while (s.paths[++s.i])
 	{
-		s.append = ft_strjoin("/", s.cmd_args[0]);
+		s.append = ft_strjoin("/", cmd);
 		s.exec_path = ft_strjoin(s.paths[s.i], s.append);
 		if (!access(s.exec_path, R_OK) && ft_exec_min(cmd) != 1)
 		{
-			ft_path_searcher_free(&s);
+			ft_arr_freer(s.paths);
 			return (s.exec_path);
 		}
 		free(s.append);
